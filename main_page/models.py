e4c6a79b02a246a0467b0763446cb86cf9d25c9b
Tony Perez <tonper19@gmail.com>
2020-05-14 20:25:42 +0200

    009. Model modification - NOT IN DB

--- main_page/models.py
+++ main_page/models.py
@@ -1,24 +1,30 @@
 from django.db import models
 from datetime import datetime
 
+# once all models are created, run the following:
+# $ python manage.py makemigrations
+# $ python manage.py migrate
+# if changes are made to the models, DO NOT MODIFY THE DB DIRECTLY, use above
+
 
 class Contact(models.Model):
     last_name = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100, blank=True)
     company = models.CharField(max_length=100)
-    email_address = models.CharField(max_length=100)
+    email_address = models.EmailField()
     telephone = models.CharField(max_length=30, blank=True)
     message = models.TextField(blank=True)
-    msg_date = models.DateTimeField(default=datetime.now, blank=True)
+    msg_date = models.DateTimeField(default=datetime.now, null=True)
     ip_address = models.CharField(max_length=50, blank=True)
     is_contacted = models.BooleanField(default=False, blank=True)
-    contact_date = models.DateTimeField(blank=True)
+    contact_date = models.DateTimeField(null=True, blank=True)
     contact_outcome = models.TextField(blank=True)
     is_contracted = models.BooleanField(default=False, blank=True)
-    contract_date = models.DateTimeField(blank=True)
+    contract_date = models.DateTimeField(null=True, blank=True)
     contract_details = models.TextField(blank=True)
     is_ok = models.BooleanField(blank=True)
     contact_details = models.TextField(blank=True)
+    # use DateTimeField(null=True, blank=True) to allow nulls and not required
 
     def __str__(self):
         name = f"{self.last_name}, {self.first_name} ({self.company})"
