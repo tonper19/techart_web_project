71d003aa580a80f943caf43211608867fe7301ca
Tony Perez <tonper19@gmail.com>
2020-05-16 16:25:06 +0200

    012. Customize Admin Display Data, Search, Fiter

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Contact
+
+# 012. Customize Admin Display Data
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'last_name', 'first_name', 'company',
+                    'email_address', 'telephone', 'msg_date', 'is_contacted',
+                    'is_contracted', 'is_ok', )
+    list_display_links = ('id', 'last_name', 'first_name',)
+    list_filter = ('company', 'msg_date', )
+    list_editable = ('is_contacted', 'is_contracted', 'is_ok', )
+    search_fields = ('last_name', 'first_name', 'company', 'email_address',
+                     'msg_date', 'is_contacted', )
+    list_per_page = 25
+
+
 # Register your models here.
 # to create a super user: $ python manage.py createsuperuser
-admin.site.register(Contact)
+admin.site.register(Contact, ContactAdmin)
